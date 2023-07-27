23350c20dd205243de6e39f5138a9a7b76db8ea4
Mello47 <melvinshuster47@gmail.com>
2023-07-27 22:09:46 +0200

    ADD component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 mallamia. All rights reserved.</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">mallamia</Link>
+      <div>
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+        <button onClick={()=> {console.log("logged out!")}}>Logout</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
