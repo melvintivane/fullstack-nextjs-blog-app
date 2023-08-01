7c3a5d70615e200a201144e6dc4ad4d658d54f50
Mello47 <melvinshuster47@gmail.com>
2023-08-01 16:59:28 +0200

    ADD social icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './footer.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 mallamia. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image className={styles.icon} src='/1.png' width={15} height={15} alt='Malla Mia'/>
+        <Image className={styles.icon} src='/2.png' width={15} height={15} alt='Malla Mia'/>
+        <Image className={styles.icon} src='/3.png' width={15} height={15} alt='Malla Mia'/>
+        <Image className={styles.icon} src='/4.png' width={15} height={15} alt='Malla Mia'/>
+      </div>
     </div>
   )
 }
