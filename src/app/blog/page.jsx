678b84fc0d16ac21796abe3b72ef61155bfd8b33
Mello47 <melvinshuster47@gmail.com>
2023-08-03 22:10:24 +0200

    UPDATED blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import styles from "./page.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href='/blog/asdfg' className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
