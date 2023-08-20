383f40968d7e4cc1563a619fa78bd276934f1f34
Mello47 <melvinshuster47@gmail.com>
2023-08-20 20:54:58 +0200

    feat: function getData()

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,138 +1,44 @@
+'use client';
+
 import React from 'react';
 import styles from "./page.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Blog = () => {
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-      <Link href='/blog/asdfg' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=""
-            width={350}
-            height={200}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}> Lorem ipsum dolor sit amet consectetur</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam 
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis, 
-            perspiciatis iure tempore sunt ad beatae suscipit impedit ipsa! Repellendus!
-            nsectetur adipisicing elit. Omnis, at velit nobis quos consectetur deserunt a eos debitis!
-          </p>
-        </div>
-      </Link>
-    </div>
+      {data.map( (item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
+              alt=""
+              width={350}
+              height={200}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
+      </div>
   );
 };
 
