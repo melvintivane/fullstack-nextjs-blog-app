dd5ec4a89a6ac4361dde61a460ffeae074599738
Mello47 <melvinshuster47@gmail.com>
2023-09-12 16:39:37 +0200

    Update implemented a cache no-store to fetch a fresh data on every fetch request

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,13 +1,14 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 import styles from "./page.module.css";
-import Image from 'next/image';
-import Link from 'next/link';
-
+import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -16,16 +17,19 @@ async function getData() {
   return res.json();
 }
 
-
 const Blog = async () => {
   const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      {data.map( (item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+      {data.map((item) => (
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
           <div className={styles.imageContainer}>
             <Image
-              src='https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
+              src="https://images.pexels.com/photos/3986923/pexels-photo-3986923.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
               alt=""
               width={350}
               height={200}
@@ -38,7 +42,7 @@ const Blog = async () => {
           </div>
         </Link>
       ))}
-      </div>
+    </div>
   );
 };
 
