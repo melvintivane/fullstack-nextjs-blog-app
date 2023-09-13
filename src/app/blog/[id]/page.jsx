832f267d9d7df476795358bf392115e4a8fbfb36
Mello47 <melvinshuster47@gmail.com>
2023-09-13 11:52:21 +0200

    Add getData fetching with useEffect

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
-import styles from './page.module.css'
-import Image from 'next/image';
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const BlogPost = () => {
+
+
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {data.title}
           </h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, at velit nobis quos 
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad iste laboriosam
-            nsectetur adipisicing elit. Omnis, at velit nobis quos 
-            consectetur deserunt a eos debitis, perspiciatis iure tempore sunt ad 
-            beatae suscipit impedit ipsa! Repellendus!
+           {data.body}
           </p>
           <div className={styles.author}>
             <Image
-              src='https://images.pexels.com/photos/16471764/pexels-photo-16471764/free-photo-of-face-rosto-cara-retrato.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              src="https://images.pexels.com/photos/16471764/pexels-photo-16471764/free-photo-of-face-rosto-cara-retrato.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>
-              Jonh Doe  
-            </span>
+            <span className={styles.username}>Jonh Doe</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src='https://images.pexels.com/photos/17781404/pexels-photo-17781404/free-photo-of-ponte-espaco-curva-luz-do-dia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            src="https://images.pexels.com/photos/17781404/pexels-photo-17781404/free-photo-of-ponte-espaco-curva-luz-do-dia.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             fill={true}
             className={styles.image}
@@ -41,8 +51,30 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nihil consectetur cupiditate eveniet natus ipsum aspernatur ab reiciendis, rem nisi fugit voluptatem quaerat a minus reprehenderit numquam temporibus. Quibusdam, modi. Ex porro laboriosam hic obcaecati excepturi sed atque dolorum quaerat soluta nulla, repellat quisquam magnam perspiciatis temporibus. Quos aliquam odit placeat veniam voluptate ipsum nihil molestias suscipit repellendus ad? Aperiam iste repudiandae ratione laborum quod accusantium aut. Repellat perspiciatis quo nobis deserunt necessitatibus eaque nisi nesciunt. Quas enim adipisci quia cum aliquam excepturi, asperiores, nihil consequuntur saepe minus ipsa amet ratione quos incidunt. Tenetur voluptates, sapiente voluptatum quis sint voluptas.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nihil consectetur cupiditate eveniet natus ipsum aspernatur ab reiciendis, rem nisi fugit voluptatem quaerat a minus reprehenderit numquam temporibus. Quibusdam, modi. Ex porro laboriosam hic obcaecati excepturi sed atque dolorum quaerat soluta nulla, repellat quisquam magnam perspiciatis temporibus. Quos aliquam odit placeat veniam voluptate ipsum nihil molestias suscipit repellendus ad? Aperiam iste repudiandae ratione laborum quod accusantium aut. Repellat perspiciatis quo nobis deserunt necessitatibus eaque nisi nesciunt. Quas enim adipisci quia cum aliquam excepturi, asperiores, nihil consequuntur saepe minus ipsa amet ratione quos incidunt. Tenetur voluptates, sapiente voluptatum quis sint voluptas.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi nihil
+          consectetur cupiditate eveniet natus ipsum aspernatur ab reiciendis,
+          rem nisi fugit voluptatem quaerat a minus reprehenderit numquam
+          temporibus. Quibusdam, modi. Ex porro laboriosam hic obcaecati
+          excepturi sed atque dolorum quaerat soluta nulla, repellat quisquam
+          magnam perspiciatis temporibus. Quos aliquam odit placeat veniam
+          voluptate ipsum nihil molestias suscipit repellendus ad? Aperiam iste
+          repudiandae ratione laborum quod accusantium aut. Repellat
+          perspiciatis quo nobis deserunt necessitatibus eaque nisi nesciunt.
+          Quas enim adipisci quia cum aliquam excepturi, asperiores, nihil
+          consequuntur saepe minus ipsa amet ratione quos incidunt. Tenetur
+          voluptates, sapiente voluptatum quis sint voluptas. Lorem ipsum dolor
+          sit amet, consectetur adipisicing elit. Modi nihil consectetur
+          cupiditate eveniet natus ipsum aspernatur ab reiciendis, rem nisi
+          fugit voluptatem quaerat a minus reprehenderit numquam temporibus.
+          Quibusdam, modi. Ex porro laboriosam hic obcaecati excepturi sed atque
+          dolorum quaerat soluta nulla, repellat quisquam magnam perspiciatis
+          temporibus. Quos aliquam odit placeat veniam voluptate ipsum nihil
+          molestias suscipit repellendus ad? Aperiam iste repudiandae ratione
+          laborum quod accusantium aut. Repellat perspiciatis quo nobis deserunt
+          necessitatibus eaque nisi nesciunt. Quas enim adipisci quia cum
+          aliquam excepturi, asperiores, nihil consequuntur saepe minus ipsa
+          amet ratione quos incidunt. Tenetur voluptates, sapiente voluptatum
+          quis sint voluptas.
         </p>
       </div>
     </div>
