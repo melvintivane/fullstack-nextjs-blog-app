f22ba75e7f1483af01ff0d8c6908769f96a5c39d
Mello47 <melvinshuster47@gmail.com>
2023-09-14 06:58:39 +0200

    Add locally data fetching and notFound function

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,59 +1,44 @@
-import React from 'react';
+import React from "react";
 import styles from "./page.module.css";
-import Button from '@/components/button/Button';
-import Image from 'next/image';
+import Button from "@/components/button/Button";
+import Image from "next/image";
+import { items } from "./data.js";
+import { notFound } from "next/navigation";
 
-const Category = ({params}) => {
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
+const Category = ({ params }) => {
+  const data = getData(params.category);
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url='#' text='See More'/>
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button url="#" text="See More" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              src={item.image}
+              alt=""
+              fill={true}
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            src='https://images.pexels.com/photos/16878811/pexels-photo-16878811/free-photo-of-onibus-grafite-graffiti-hollywood.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=''
-            fill={true}
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url='#' text='See More'/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            src='https://images.pexels.com/photos/16878811/pexels-photo-16878811/free-photo-of-onibus-grafite-graffiti-hollywood.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=''
-            fill={true}
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button url='#' text='See More'/>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image 
-            className={styles.img}
-            src='https://images.pexels.com/photos/16878811/pexels-photo-16878811/free-photo-of-onibus-grafite-graffiti-hollywood.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load'
-            alt=''
-            fill={true}
-          />
-        </div>
-      </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Category;
