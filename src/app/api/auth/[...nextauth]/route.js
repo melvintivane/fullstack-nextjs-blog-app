666fb8981d1a918bf1eef2ccf116164291f16c52
Mello47 <melvinshuster47@gmail.com>
2023-09-16 08:05:27 +0200

    Create nextauth route

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
