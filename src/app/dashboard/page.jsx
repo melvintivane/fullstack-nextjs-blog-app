1740101000786ff3eafe948059c0b6c11801830a
Mello47 <melvinshuster47@gmail.com>
2023-09-18 06:43:46 +0200

    Add useSession and useRouter to check user status and redirect user

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,11 +3,17 @@
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
   // const [data, setData] = useState();
   // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState(false);
+
+  const session = useSession();
+
+  const router = useRouter();
 
   //Client-side data fetching with useEffect
   // useEffect(() => {
@@ -30,6 +36,8 @@ const Dashboard = () => {
   //   getData();
   // }, []);
 
+  
+
   //Client-side data fetching with SWR
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -38,16 +46,22 @@ const Dashboard = () => {
       "https://jsonplaceholder.typicode.com/posts",
       fetcher
     );
-
-    if (error) return <div>failed to load</div>;
-    if (isLoading) return <div>loading...</div>;
-
-    console.log(data);
   }
 
-  Profile();
+  if (session.status === "loading") {
+    //todo add skeleton or progress bar
+    return <p>Loading</p>;
+  }
 
-  return <div className={styles.container}>Dashboard</div>;
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  
+
+  if (session.status === "authenticated") {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
