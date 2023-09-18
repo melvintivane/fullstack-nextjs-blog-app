0e511c6b78c27d295aa174685ad690c9474f7b19
Mello47 <melvinshuster47@gmail.com>
2023-09-18 06:40:36 +0200

    Add CredentialsProvider to auth user using email and password

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +11,39 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          //verificar user
+          const user = await User.findOne({ email: credentials.email });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User Not Found!");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
